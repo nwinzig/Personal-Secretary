e1a50f7a271f0c51b1b8d962ced6848588e4511d
Noah <noahwinzig@gmail.com>
2023-05-16 15:59:23 -0400

    adding file structure and notes for front end

---
+++ react-app/src/splash.tsx
@@ -0,0 +1,17 @@
+
+
+function Splash(){
+    return(
+        <div>
+            <h1>Personal Secretary</h1>
+            <ol>
+                <li>Notes</li>
+                <li>Calendar</li>
+                <li>Profile</li>
+                <li>Home</li>
+            </ol>
+        </div>
+    )
+}
+
+export default Splash;
