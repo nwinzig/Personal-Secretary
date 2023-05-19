da3d61037a2c5eabd6dbb59b2f4cdf8d72140000
Noah <noahwinzig@gmail.com>
2023-05-19 09:48:08 -0400

    installed swiper and attempting to use it to work out a 3d carousel

---
+++ react-app/src/carousel/Carousel.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Swiper, SwiperSlide} from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+import { EffectCoverflow,Pagination,Navigation} from 'swiper'
+
+function Carousel(){
+    return (
+        <div className='carouselContainers'>
+
+        </div>
+    )
+}
