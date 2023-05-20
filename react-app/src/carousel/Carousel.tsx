79ef62582bdd6a26159856b2e2505fad497aac0b
Noah <noahwinzig@gmail.com>
2023-05-20 07:31:53 -0400

    testing using swiper to build a 3d carousel

--- react-app/src/carousel/Carousel.tsx
+++ react-app/src/carousel/Carousel.tsx
@@ -1,17 +1,36 @@
 import React from "react";
 import {Swiper, SwiperSlide} from 'swiper/react'
+import { EffectCoverflow, Navigation, Pagination} from 'swiper'
 
 import 'swiper/css'
 import 'swiper/css/effect-coverflow'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
+// import '../../node_modules/swiper/css'
 
-import { EffectCoverflow,Pagination,Navigation} from 'swiper'
+// // test images to fill carousel until I can figure out how to fill them with sample of the users page
+
+// import slide_image1 from '../assets/img1.png'
+// import slide_image2 from '../assets/img2.png'
+// import slide_image3 from '../assets/img3.png'
 
 function Carousel(){
     return (
-        <div className='carouselContainers'>
-
-        </div>
+        <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination= {{clickable: true}}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+        </Swiper>
     )
 }
+
+export default Carousel
--- react-app/src/splash/splash.tsx
+++ react-app/src/splash/splash.tsx
@@ -1,14 +1,16 @@
+import Carousel from '../carousel/Carousel';
 import './splash.css'
 
 function Splash(){
     return(
         <div className='splashContainer'>
-            <ol>
+            {/* <ol>
                 <li>Notes</li>
                 <li>Calendar</li>
                 <li>Profile</li>
                 <li>Home</li>
-            </ol>
+            </ol> */}
+            <Carousel />
         </div>
     )
 }
